7ee37bab174ae07e33e279b5e96ab15157c7cd36
Antonius <jordan_smith47@yahoo.com>
2024-05-27 23:52:56 -0500

    added files

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb+srv://jordanbryantsmith:Shadowfax1234!@cluster0.ebgyrrt.mongodb.net/',(err,client) =>{
+
+})
 
 app.listen(3000,function(){
     console.log('Bahaha your server is lestening on 3000')
+})
+app.set('view engine','ejss')
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
