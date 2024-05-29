32e6bd55a681474ff7f19c9cbbf8c484519e83dd
Antonius <jordan_smith47@yahoo.com>
2024-05-29 18:54:51 -0500

    added files

--- public/main.js
+++ public/main.js
@@ -1,5 +1,18 @@
 const update = document.querySelector('#update-button')
 
 update.addEventListener('click', _ =>{
-    
+
 })
+
+function shuffledArray(){
+const shuffle = (array) => {
+    let oldElement;
+    for (let i = array.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      oldElement = array[i];
+      array[i] = array[rand];
+      array[rand] = oldElement;
+    }
+    return array;
+  } 
+}
