b0f98e7f7f71ebcff55f52937ffb7b5baf647618
Antonius <jordan_smith47@yahoo.com>
2024-05-29 14:47:12 -0500

    added files

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://jordanbryantsmith:Shadowfax1234@cluster0.ebgyrrt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
-MongoClient.connect('mongodb+srv://jordanbryantsmith:Shadowfax1234!@cluster0.ebgyrrt.mongodb.net/',(err,client) =>{
+MongoClient.connect(connectionString)
+    .then(client => {
 
+        console.log('Connected to Database');
+        const db = client.db('Gazerbeam2')
+
+        app.listen(3000,function(){
+        console.log('Bahaha your server is listening on 3000')
+})
+        app.set('view engine','ejss')
+
+        app.get('/', (req,res) => {
+         res.sendFile(__dirname + '/index.html')
+         app.get('/', (req, res) => {
+            db.collection('Major-Arcana')
+              .find()
+              .toArray()
+              .then(results => {
+                console.log(results)
+              })
+              .catch(error => console.error(error))
+            // ...
+          })
 })
 
-app.listen(3000,function(){
-    console.log('Bahaha your server is lestening on 3000')
-})
-app.set('view engine','ejss')
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    })
+    .catch(error => console.error(err));
 
