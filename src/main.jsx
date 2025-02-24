70a24a9363f1d4a06289a2a717f6790cd008dd91
Jordan Bryant-Smith <134249845+Gazerbeam2@users.noreply.github.com>
2025-02-23 22:55:38 -0600

    fix: index.html loading

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
